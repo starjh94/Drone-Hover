f40c1e063efab34b79a0fd7351ede835c9edc528
starjh94 <starjh94@users.noreply.github.com>
2017-04-17 17:51:00 +0000

    Using theading Timer(every 5 sec)

---
+++ Qlearn_proto/inc_servo.py
@@ -0,0 +1,40 @@
+import Servo
+import degree
+import threading
+
+
+pwm_1 = 1.1
+pwm_2 = 1.22
+
+def every5sec() :
+    b = degree.acc()
+    
+    global pwm_1
+    pwm_1 += 0.01
+    
+    print "pwm_v1 = %s pwm_v2 = %s degree = %s \n" % (pwm_1, pwm_2, b.pitch())
+    threading.Timer(5, every5sec).start()
+
+
+def main() :
+    a = Servo.servo()
+    b = degree.acc()
+    
+    f = open("data.txt", 'w')
+    
+    every5sec()
+    while(True):
+        a.servo_1(pwm_1)
+        a.servo_2(pwm_2)
+        
+        data = "pwm_v1 = %s pwm_v2 = %s degree = %s \n" % (pwm_1, pwm_2, b.pitch())
+        f.write(data)
+        
+    #pitch_v = b.pitch
+    #print b.pitch()
+    
+if __name__ == '__main__':
+    main()
+	
+	
+
