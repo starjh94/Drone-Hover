56d02ea2d24b18f3943f61bc111aeaef9405a8fe
starjh94 <starjh94@users.noreply.github.com>
2017-04-17 13:38:50 +0000

    change ince_servo.py file

--- Qlearn_proto/ince_servo.py
+++ Qlearn_proto/ince_servo.py
@@ -1,11 +1,32 @@
+import Servo
 import degree
 import time
-a = degree.acc()
+a = Servo.servo()
+b = degree.acc()
+pitch_v = 0
+start_time = 0
+end_time = 0
+pwm_1 = 1.1
+pwm_2 = 1.22
+f = open("data.txt", 'w')
 
 while(True):
+        a.servo_1(pwm_1)
+        a.servo_2(pwm_2)
+	data = "pwm_v1 = %s pwm_v2 = %s degree = %s \n" % (pwm_1, pwm_2, b.pitch())
+	f.write(data)
+
+	if(start_time == 0):
+        	start_time = time.time()
+       # print b.pitch()
+        end_time = time.time()
+        
+	if(end_time - start_time >= 5):
+        	pwm_1 += 0.01
+		start_time = 0
+		start_time = 0
+		print pwm_1
+	pitch_v = b.pitch
+#	print b.pitch()
 	
-	v = 0 
-	for _ in range(10):
-		v +=a.pitch()
-	print v/10
-	time.sleep(0.1)
+	
