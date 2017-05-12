b58a3daac756409a815d63809bc382a818120b94
starjh94 <starjh94@users.noreply.github.com>
2017-05-12 07:58:22 +0000

    upload time check file

--- Qlearn_proto/que_inc_servo.py
+++ Qlearn_proto/que_inc_servo.py
@@ -1,7 +1,7 @@
 import Servo
 import degree
 import threading
-
+import time
 ## Initialize
 pwm_1 = 1.1
 pwm_2 = 1.22
@@ -23,19 +23,21 @@ def main() :
         b = degree.acc()
 
         f = open("data.txt", 'w')
-
+	count = 0
 	que = []
-
+	que_time = []
         every5sec()
         while(True):
 	        a.servo_1(pwm_1)
                 a.servo_2(pwm_2)
-                
                 que.append(b.pitch())
-                
+                que_time.append(time.time())
+		if(count >=1):
+			print "sensor time == " , que_time[count]-que_time[count-1]
+		count +=1
                 if(len(que) == 100):
                         average_pitch = sum(que,0.0) /len(que)
-        		print "pwm_v1 = %s pwm_v2 = %s degree = %s" % (pwm_1, pwm_2, average_pitch)
+        		print "pwm_v1 = %s pwm_v2 = %s degree = %s " % (pwm_1, pwm_2, average_pitch)
                	 	data = "pwm_v1 = %s pwm_v2 = %s degree = %s \n" % (pwm_1, pwm_2, average_pitch)
                  	f.write(data)
 	                #que.pop(0)
