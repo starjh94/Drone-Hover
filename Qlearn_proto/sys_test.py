345af1e20ef0e9828bab1ef11f4210c0dbb117dd
TaeWoo21 <lbaksa21@gmail.com>
2017-10-31 02:01:17 +0000

    2017-10-31 model save 구현

---
+++ Qlearn_proto/sys_test.py
@@ -0,0 +1,37 @@
+import os
+import sys
+
+if len(sys.argv) == 2 :
+
+	if os.path.exists("./TF_Data/"+sys.argv[1]) is False :
+		print "'%s' is Not exist file\n" %(sys.argv[1])
+		exit()
+	else:
+		print "'%s' model will be restored!\n" %(sys.argv[1])
+while True:
+	learning_model_name= raw_input("Model name for save(<ex> *.ckpt) : ")
+
+	if learning_model_name[-5:] != ".ckpt":
+		print "\nPlease write correctly!"
+	elif os.path.exists("./TF_Data/"+learning_model_name) is True:	
+		
+		while True:
+			same_name_answer = raw_input("\nThe file name already exists.\nDo you want to overwrite?(Y / N): ")
+		
+			if same_name_answer.upper() == "Y":
+				print "\nIt will be saved as '%s'" % (learning_model_name)
+				break
+			elif same_name_answer.upper() == "N":
+				print "\nEnter the model name again"
+				break
+			else:
+				print "\nPleas write correctly!"
+
+		if same_name_answer.upper() == "Y":
+			break
+	else:	
+		print "\nIt will be saved as '%s'" % (learning_model_name)
+		break
+
+
+
